2d7df58d4507d21c9f8c7b3ee1cbb6f305c9da2d
George Watson <gigolo@hotmail.co.uk>
2025-02-15 16:48:01 +0000

    :)

--- config.fish
+++ config.fish
@@ -18,19 +18,19 @@ function fish_mode_prompt
     switch $fish_bind_mode
         case default
             set_color --bold red
-            echo 'N'
+            echo ' N'
         case insert
             set_color --bold green
-            echo 'I'
+            echo ' I'
         case replace_one
             set_color --bold yellow
-            echo 'R'
+            echo ' R'
         case visual
             set_color --bold brmagenta
-            echo 'V'
+            echo ' V'
         case '*'
             set_color --bold red
-            echo '?'
+            echo ' ?'
     end
     set_color normal
 end
